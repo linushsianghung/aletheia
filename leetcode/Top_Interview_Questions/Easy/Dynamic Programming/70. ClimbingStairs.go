f23fdb705a87923c9b460cd646a3ffae1bcefa34
Hsiang Hung <linushung0325@gmail.com>
2024-07-19 22:05:58 +0800

    Udpate Solution

--- leetcode/Top_Interview_Questions/Easy/Dynamic Programming/70. ClimbingStairs.go
+++ leetcode/Top_Interview_Questions/Easy/Dynamic Programming/70. ClimbingStairs.go
@@ -1,39 +1,46 @@
 package Dynamic_Programming
 
 // https://leetcode.com/problems/climbing-stairs/
-// Ref:
-// - https://leetcode.com/problems/climbing-stairs/solutions/25299/basically-it-s-a-fibonacci/
-// - https://www.youtube.com/watch?v=RrFg9SZ8VoM
-// - https://www.youtube.com/watch?v=Y0lT9Fck7qI
+// Ref: https://leetcode.com/problems/climbing-stairs/solutions/25299/basically-it-s-a-fibonacci/
 /*
 You are climbing a staircase. It takes n steps to reach the top.
 
 Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 */
 func climbStairs(n int) int {
-	return climStairsDP(n)
+	return climStairsMemoriseDP(n, make(map[int]int))
 }
 
-func climStairsDP(n int) int {
-	// First, define Base Cases
-	if n <= 1 {
+/*
+Time Complexity O(2^n) of Depth First Search (DFS) where "2" is derived from the ways to climb (2 decision tree) and n of the height of the tree
+Time Complexity O(η^n) where η is Fibonacci constant := 1.83
+Time Limit Exceeded!!!
+*/
+func climStairsRecursivelyImpl(n int) int {
+	switch n {
+	case 1:
 		return 1
-	}
-	if n == 2 {
+	case 2:
 		return 2
+	default:
+		return climStairsRecursivelyImpl(n-1) + climStairsRecursivelyImpl(n-2)
+	}
+}
+
+func climStairsMemoriseDP(n int, memo map[int]int) int {
+	if _, ok := memo[n]; ok {
+		return memo[n]
 	}
 
-	// For Simplest Case of N = 3
-	waysToTwoStepBeforeN := 1
-	waysToOneStepBeforeN := 2
-
-	for i := 3; i < n; i++ {
-		waysToNStairs := waysToTwoStepBeforeN + waysToOneStepBeforeN
-		waysToTwoStepBeforeN = waysToOneStepBeforeN
-		waysToOneStepBeforeN = waysToNStairs
+	switch n {
+	case 1:
+		return 1
+	case 2:
+		return 2
+	default:
+		memo[n] = climStairsMemoriseDP(n-1, memo) + climStairsMemoriseDP(n-2, memo)
+		return memo[n]
 	}
-
-	return waysToTwoStepBeforeN + waysToOneStepBeforeN
 }
 
 func climStairsBottomUpDP(n int) int {
@@ -56,18 +63,25 @@ func climStairsBottomUpDP(n int) int {
 	return tabu[n]
 }
 
-/*
-Time Complexity O(2^n) of Depth First Search (DFS) where "2" is derived from the ways to climb (2 decision tree) and n of the height of the tree
-Time Complexity O(η^n) where η is Fibonacci constant := 1.83
-Time Limit Exceeded!!!
-*/
-func climStairsRecursivelyImpl(n int) int {
-	switch n {
-	case 1:
+// Too Complicated....
+func climStairsIteratively(n int) int {
+	// First, define Base Cases
+	if n <= 1 {
 		return 1
-	case 2:
-		return 2
-	default:
-		return climStairsRecursivelyImpl(n-1) + climStairsRecursivelyImpl(n-2)
 	}
+	if n == 2 {
+		return 2
+	}
+
+	// For Simplest Case of N = 3
+	waysToTwoStepBeforeN := 1
+	waysToOneStepBeforeN := 2
+
+	for i := 3; i < n; i++ {
+		waysToNStairs := waysToTwoStepBeforeN + waysToOneStepBeforeN
+		waysToTwoStepBeforeN = waysToOneStepBeforeN
+		waysToOneStepBeforeN = waysToNStairs
+	}
+
+	return waysToTwoStepBeforeN + waysToOneStepBeforeN
 }
