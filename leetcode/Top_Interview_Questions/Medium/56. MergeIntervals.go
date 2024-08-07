2ff1a5a10162f75d9c55f109bf5a1d7c36c26569
Hsiang Hung <linus@Hsiangs-MacBook-Pro.local>
2024-08-07 14:49:08 +0800

    Update Solutions

--- leetcode/Top_Interview_Questions/Medium/56. MergeIntervals.go
+++ leetcode/Top_Interview_Questions/Medium/56. MergeIntervals.go
@@ -1,10 +1,10 @@
-package Mediium
+package Medium
 
 // https://leetcode.com/problems/merge-intervals/
 /*
-Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, 
+Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals,
 and return an array of the non-overlapping intervals that cover all the intervals in the input.
 */
 func merge(intervals [][]int) [][]int {
-    
+	return nil
 }
